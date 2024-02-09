7b9993f2446a5f4328c06312a3cb438ec044d6a1
MohammedGhaly <medooghaly7@gmail.com>
2024-02-10 01:15:41 +0200

    setting up Fake Authentication context

--- contexts/FakeAuthContext.jsx
+++ contexts/FakeAuthContext.jsx
@@ -1,9 +1,47 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 
 const AuthContext = createContext();
 
+const initialState = { user: null, isAuthenticated: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, isAuthenticated: true, user: action.payload };
+    case "logout":
+      return initialState;
+    default:
+      throw new Error("unknown action");
+  }
+}
+
 function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
@@ -13,4 +51,4 @@ function useAuth() {
   return context;
 }
 
-export default useAuth;
+export { useAuth, AuthProvider };
